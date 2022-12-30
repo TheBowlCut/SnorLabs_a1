881d93e3d0902f42033fc8592f122cf3e1d06073
KBJ-55 <76529236+KBJ-55@users.noreply.github.com>
2022-12-30 15:20:40 +0000

    30 Dec 2022 Main activity with button functionality If no user permission, button will send to permission activity If user permission, button will send to blank timer activity.

---
+++ src/main/java/com/kristianjones/snorlabs_a1/DynTimerActivity.java
@@ -0,0 +1,15 @@
+package com.kristianjones.snorlabs_a1;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DynTimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dtimer);
+    }
+}
