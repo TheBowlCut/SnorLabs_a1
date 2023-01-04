dd8ee9cbf484c40142365ff472a346f2b919534e
KBJ-55 <76529236+KBJ-55@users.noreply.github.com>
2023-01-04 19:40:41 +0000

    Added activity for timer No code lined up, just visual.

--- src/main/java/com/kristianjones/snorlabs_a1/DynTimerActivity.java
+++ src/main/java/com/kristianjones/snorlabs_a1/DynTimerActivity.java
@@ -1,15 +1,32 @@
 package com.kristianjones.snorlabs_a1;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DynTimerActivity extends AppCompatActivity {
 
+    // Generic tag as Log identifier
+    static final String TAG = MainActivity.class.getName();
+
+    Spinner settingSpinner;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dtimer);
+
+        // Declare all variables
+        settingSpinner = findViewById(R.id.optionsSpinner2);
+
+        // Set settings array adaptor, linked to the 'settings' string in strings.xml
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.settings, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        //Set spinner to arrayAdaptor
+        settingSpinner.setAdapter(adapter);
     }
 }
